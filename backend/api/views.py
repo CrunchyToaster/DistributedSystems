c79694f178c6e564db92455a188e7c38f23ec6df
CrunchyToaster <89310389+CrunchyToaster@users.noreply.github.com>
2025-06-14 13:42:33 +0000

    Add kubernetes for staging

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .models import Item
 from .serializers import ItemSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    authentication_classes = []
+    permission_classes     = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         data = request.data
