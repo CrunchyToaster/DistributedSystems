306e8bf75a87b3907fbd0fe139a0ba2701607ca4
CrunchyToaster <89310389+CrunchyToaster@users.noreply.github.com>
2025-06-26 22:00:30 +0000

    changed url settings agains, works now in docker-compose

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet
 
-router = DefaultRouter(trailing_slash=False)   # ← no “/” at the end
+router = DefaultRouter(trailing_slash=True)   # ← no “/” at the end
 # equivalent:  SimpleRouter(trailing_slash=False)
 
 router.register(r'', ItemViewSet, basename='items')
